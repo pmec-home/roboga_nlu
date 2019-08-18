0e952062adf0f874fb3cd7756f589f8ab79c387f
Eduardo Garcia <edusantosgarcia@gmail.com>
2019-08-18 16:02:18 -0300

    Converted to ROS Service

--- src/rasa_interpreter.py
+++ src/rasa_interpreter.py
@@ -3,22 +3,25 @@ import os
 
 import rospy
 from std_msgs.msg import String
-from roboga_nlu.msg import Nlu
+from roboga_nlu.srv import Nlu, NluResponse
 from rasa_nlu.model import Interpreter
 
 if __name__ == "__main__":
     directory = os.path.dirname(os.path.realpath(__file__))
     rospy.init_node('rasa_nlu', anonymous=True)
     interpreter = Interpreter.load(directory+"/rasa/models/nlu")
-    pub = rospy.Publisher("roboga/nlu/result", Nlu, queue_size=10)
-    def callback(data):
-        msg = Nlu()
-        result = interpreter.parse(data.data)
-        msg.message = str(result['text'])
-        msg.intent = str(result["intent"].get("name"))
-        msg.entities = ["{{entity:{0} , value:{1}}}".format(x.get("entity"), x.get("value")) for x in result.get("entities")]
-        pub.publish(msg)
+    
+    def handler(req):
+        result = interpreter.parse(req.data)
+        message = str(result['text'])
+        intent = str(result["intent"].get("name"))
+        entities = ["{{\"entity\":\"{0}\", \"value\":\"{1}\"}}".format(x.get("entity"), x.get("value")) for x in result.get("entities")]
+        return NluResponse(
+            message=message,
+            intent=intent,
+            entities=entities
+        )
 
-    rospy.Subscriber("roboga/stt/result", String, callback=callback)
+    pub = rospy.Service("roboga/nlu", Nlu, handler)
     rospy.spin()
     
